b1ce734ad313f40ac20b3bc2eca6190f8c12385b
Jonas Jakobsson <jonas@knacka.se>
2018-10-17 22:15:56 +0200

    added nuspec

--- Example/AspNetMvcSubResourceIntegrityHelper/Properties/AssemblyInfo.cs
+++ Example/AspNetMvcSubResourceIntegrityHelper/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AspNetMvcSubResourceIntegrityHelper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A helper that will calculate SRI integrity tags for you and cache locally in file")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AspNetMvcSubResourceIntegrityHelper")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2018 Jonas Jakobsson")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
---
+++ Example/ClassLibrary1/ConfigHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetMvcSubResourceIntegrityHelper.Config
+{
+	public static class ConfigHelper
+	{
+		public static bool SriThrowIfUsed => string.Compare(ConfigurationManager.AppSettings["SriHelper_ThrowIfUsed"], "true",
+												 StringComparison.InvariantCultureIgnoreCase) == 0;
+
+		public static string SriFilePath => ConfigurationManager.AppSettings["SriHelper_FilePath"];
+		public static bool SriDownloadIfNotPresentInFile => string.Compare(ConfigurationManager.AppSettings["SriHelper_DownloadIfNotFoundInFile"], "true",
+												 StringComparison.InvariantCultureIgnoreCase) == 0;
+
+		public static string SriInsteadOfTilde => ConfigurationManager.AppSettings["SriHelper_BasePath"];
+	}
+}
